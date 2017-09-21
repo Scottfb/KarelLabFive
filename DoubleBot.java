829ef7ee9b00263215415821184f19437c217a08
Scottfb <31323099+Scottfb@users.noreply.github.com>
2017-09-21 11:37:38 -0700

    LabFive Done

--- DoubleBot.java
+++ DoubleBot.java
@@ -12,18 +12,37 @@ import kareltherobot.*;
 public class DoubleBot extends Robot
 {
     // instance variables - replace the example below with your own
-    public int beepers;
-
+    public int beepers = 0;
     /**
      * Constructor for objects of class Template
      */
      public DoubleBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
-
     public void doubleBeepers()
     {
-        // put your code here
-        
+        amountBeepers();
+        replaceFirstPile();
+        placeDoubleBeepers();
+    }
+    public void amountBeepers() {
+        move();
+        while (nextToABeeper()) {
+            pickBeeper();
+            beepers ++;
+        }
+    }
+    public void replaceFirstPile() {
+        for (int y = beepers; y > 0; y--) {
+            putBeeper();
+        }
+    }
+    public void placeDoubleBeepers() {
+        move();
+        for (int x = 2 * beepers; x > 0; x--) {
+            putBeeper();
+        }
+        turnLeft();
+        move();
     }
 }
